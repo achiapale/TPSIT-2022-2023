f0aec4869f27e60375337ab675f911eca7c3b6a3
unknown <asia.chiapale@itiscuneo.eu>
2022-09-17 19:09:11 +0100

    chat UDP with input and name

--- 001_ComunicazioneBroadcastUDP/client.py
+++ 001_ComunicazioneBroadcastUDP/client.py
@@ -2,12 +2,13 @@ from socket import AF_INET, SOCK_DGRAM, socket
 
 PORT = input("Inserisci il numero della porta: ")
 HOST = input("Inserisci l'indirizzo dell'host: ")
+NAME = input("Inserire il proprio nome: ")
 
 def chatClient():
     with socket(AF_INET, SOCK_DGRAM) as s:
         msg = "hello world"
         msg = msg.encode('utf8')
-        s.sendto(msg, (f"{HOST}", int(PORT)))
+        s.sendto(msg, (f"{HOST}", int(PORT)), f"{NAME}")
 
 
 if __name__ == "__main__":
