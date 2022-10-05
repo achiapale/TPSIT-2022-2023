5b6a49eef05f2dbbfe96e9a76e66e286dd4830f5
unknown <asia.chiapale@itiscuneo.eu>
2022-10-05 16:11:00 +0100

    aggiunta della classe packet

--- 001_ComunicazioneBroadcastUDP/client.py
+++ 001_ComunicazioneBroadcastUDP/client.py
@@ -1,15 +1,23 @@
 from socket import AF_INET, SOCK_DGRAM, socket
+from packet import *
 
-def chatClient(HOST, PORT, MSG):
+
+def chatClient(HOST, PORT, NAME):
+    running = True
     with socket(AF_INET, SOCK_DGRAM) as s:
-        MSG = MSG.encode('utf8')
-        s.sendto(MSG, (f"{HOST}", int(PORT)))
-
+        while running == True:
+            msg = input("Inserire il messaggio. Se vuoi uscire, digitare 'exit': ")
+            if(msg != "exit"):
+                running = False
+            else:
+                pkt = Packet(NAME, MSG)
+                s.sendto(pkt.to_bytes(), HOST, PORT)
 
 
 if __name__ == "__main__":
     HOST = input("Inserisci l'indirizzo dell'host: ")
     PORT = input("Inserisci il numero della porta: ")
-    MSG = input("Inserire il messaggio: ")
+    NAME = input("Inserire il nome: ")
+    
     while True:
-        chatClient(HOST, PORT, MSG)
+        chatClient(HOST, PORT, NAME)
--- 001_ComunicazioneBroadcastUDP/server.py
+++ 001_ComunicazioneBroadcastUDP/server.py
@@ -1,8 +1,11 @@
 from socket import AF_INET, SOCK_DGRAM, socket
+from packet import *
 
 #byte che voglio leggere
 BUFFER_SIZE = 1024
 
+HOST = "127.0.0.1"
+PORT = 5000
 # interfaccia su cui vogiamo ascoltare
 # possibilità:  1. localhost (127.0.0.1)
 #               2. indirizzo di una mia interfaccia -> utile perchè se si hanno più schede di rete si hanno più ip
@@ -12,21 +15,19 @@ BUFFER_SIZE = 1024
 # oggetto di tipo bytes -> array di byte
 
 def chatServer(HOST, PORT):
+    running = True
     # quando si esce da with il socket viene automaticamente chiuso
     # non serve al fondo mettere s.close()
     with socket(AF_INET, SOCK_DGRAM) as s:
         # nel bind si passa una tupla, non si possono aggiungere altri elementi
-        s.bind((f"{HOST}", int(PORT)))
-        msg = s.recvfrom(BUFFER_SIZE)
-        # msg è un bytes, ma noi lo vogliamo in stringa
-        msg = msg[0].decode('utf8')
-        print(msg)
+        s.bind((HOST, PORT))
+        while running == True:
+            msg = s.recvfrom(BUFFER_SIZE)
+            print(msg)
+            pkt = Packet.from_bytes(msg[0])
+            print(f"l'username: ", {pkt.username}, " ha inviato il messaggio: ", {pkt.message})
 
 
 # eseguito solo da terminale
 if __name__ == "__main__":
-    HOST = input("Inserire l'indirizzo dell'host: ")
-    # > di 1024
-    PORT = input("Inserire il numero della porta: ")
-    while True:
-        chatServer(HOST, PORT)
+    chatServer(HOST, PORT)
